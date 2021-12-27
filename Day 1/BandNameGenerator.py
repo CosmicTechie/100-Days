44990978e18ee0b1325e8eb519e43dc984ee2ba5
Nishant Kumar <62173480+CosmicTechie@users.noreply.github.com>
2021-12-27 13:00:31 +0530

    Add files via upload

---
+++ Day 1/BandNameGenerator.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to the Brand Name Generator")
+#2. Ask the user for the Nationality.
+nationality=input("What is your Nationality?\n")
+#3. Ask the user for the something that user likes the most.
+monument=input("What is one thing that you like the most? \n")
+#4. Combine the Nationality and most liked thing,  and show them their band name.
+print("Your Band Name could be "+nationality + " " + monument)
